3cfb86add95890e34c8635fd197a48628fc1a984
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-07-03 19:50:00 +0300

    Fixed page.tsx in app/notes/[id]

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,11 +2,7 @@ import { HydrationBoundary, dehydrate, QueryClient } from '@tanstack/react-query
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from "./NoteDetails.client";
 
-interface NoteDetailsPageProps {
-  params: { id: string };
-}
-
-export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+export default async function NoteDetailsPage({ params }: { params: { id: string } }) {
   const id = Number(params.id);
 
   const queryClient = new QueryClient();
